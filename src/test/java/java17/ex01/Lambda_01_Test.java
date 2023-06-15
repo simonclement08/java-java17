2e049422a4f8ef7b46353eded29f42cfd501a532
Clément <simon.clement.pierre.daniel@gmail.com>
2023-06-15 11:21:44 +0200

    TP2

--- src/test/java/java17/ex01/Lambda_01_Test.java
+++ src/test/java/java17/ex01/Lambda_01_Test.java
@@ -1,5 +1,6 @@
 package java17.ex01;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java17.data.Data;
@@ -39,8 +40,7 @@ public class Lambda_01_Test {
 
         List<Person> personList = Data.buildPersonList(100);
 
-        // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p -> p.getAge() >= 18);
 
         assert result.size() == 83;
 
@@ -56,8 +56,7 @@ public class Lambda_01_Test {
 
         List<Person> personList = Data.buildPersonList(100);
 
-        // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p -> "first_10".equals(p.getFirstname()));
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -73,9 +72,8 @@ public class Lambda_01_Test {
 
         String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
 
-        // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
-        // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        PersonPredicate personPredicate = p -> p.getAge() > 49 && passwordSha512Hex.matches(DigestUtils.sha512Hex(p.getPassword())) ;
+        List<Person> result = filter(personList, personPredicate);
 
         assert result.size() == 6;
         for (Person person : result) {
