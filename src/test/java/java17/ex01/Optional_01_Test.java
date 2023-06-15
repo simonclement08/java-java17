9a47ef36f813c2b6ae41e631f6c2b1226224fe1e
Clément <simon.clement.pierre.daniel@gmail.com>
2023-06-15 12:49:10 +0200

    TP4

--- src/test/java/java17/ex01/Optional_01_Test.java
+++ src/test/java/java17/ex01/Optional_01_Test.java
@@ -23,28 +23,21 @@ public class Optional_01_Test {
 
     @Test
     public void test_optional_ifPresent() throws Exception {
-    	
     	List<Person> persons = Data.buildPersonList(100);
 
-        // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 18 ans
-        // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream().filter(person -> person.getAge() == 18).findFirst();
         assertThat(optPerson.isPresent(), is(true));
         
-        // TODO afficher la personne en question si l'optional contient une personne
+        optPerson.ifPresent(person -> System.out.println(person));
     }
 
     @Test(expected=NotPresentException.class)
     public void test_optional_notPresent() throws Exception {
     	List<Person> persons = Data.buildPersonList(50);
 
-        // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 75 ans
-        // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream().filter(person -> person.getAge() == 75).findFirst();
         assertThat(optPerson.isPresent(), is(false));
         
-        // TODO si la personne n'existe pas, jeter une exception NotPresentException
-        // TODO utiliser la méthode "orElseThrow"
-
+        optPerson.orElseThrow(NotPresentException::new);
     }
 }
